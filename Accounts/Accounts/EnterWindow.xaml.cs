e73cd1997d92c83771b516d83e355ca46b8ca408
xomem <35069727+xomem@users.noreply.github.com>
2018-03-17 14:31:49 +0300

    fdsafas

---
+++ Accounts/Accounts/EnterWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Accounts
+{
+    /// <summary>
+    /// Interaction logic for EnterWindow.xaml
+    /// </summary>
+    public partial class EnterWindow : Window
+    {
+        public EnterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void enterButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(loginBox.Text != "" && passwordBox.Password != "")
+            {
+                if (Convert.ToString(errorLabel.Content) != "")
+                {
+                    errorLabel.Content = "";
+                }
+                if(Querys.Enter(loginBox.Text, passwordBox.Password))
+                {
+                    MessageBox.Show("true");
+                }
+                else
+                {
+                    MessageBox.Show("false");
+                }
+            }
+            else
+            {
+                errorLabel.Content = "Не все поля заполнены!";
+            }
+        }
+    }
+}
---
+++ Accounts/Accounts/Querys.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Data;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Accounts
+{
+    public static class Querys
+    {
+        const string ConnectionAdres = @"data source=(LocalDB)\MSSQLLocalDB;attachdbfilename=|DataDirectory|\MainDatabase.mdf;MultipleActiveResultSets=True;";
+
+        public static bool Enter(string login, string password)
+        {
+            var query = $"SELECT * FROM [Пользователи] WHERE [Пользователи].Логин = N'{login}' AND [Пользователи].Пароль = N'{password}';";
+            bool result = false;
+            using (SqlConnection connection = new SqlConnection(ConnectionAdres))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                result = command.ExecuteNonQuery() >= 1;
+                connection.Close();
+            }
+            return result;
+        }
+    }
+}
